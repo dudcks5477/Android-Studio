ac01f2ad6930d811698dde404c13618b87c06cf4
dudcks5477 <dudcks5477@naver.com>
2022-08-16 12:00:46 +0900

    Intent Practice

--- app/src/main/java/com/example/myapplication/Intent1.kt
+++ app/src/main/java/com/example/myapplication/Intent1.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_intent.*
 
 class Intent1 : AppCompatActivity() {
@@ -11,12 +12,12 @@ class Intent1 : AppCompatActivity() {
         setContentView(R.layout.activity_intent)
 
         change_activity.setOnClickListener {
-            val intent = Intent(this@Intent1, Intent2::class.java)
-
-            // Key, Value 방식 -> Key와 value를 쌍으로 만들어 저장한다. -> Dictionary
-            intent.putExtra("number1", 1)
-            intent.putExtra("number2", 2)
-            startActivity(intent)
+//            val intent = Intent(this@Intent1, Intent2::class.java)
+//
+//            // Key, Value 방식 -> Key와 value를 쌍으로 만들어 저장한다. -> Dictionary
+//            intent.putExtra("number1", 1)
+//            intent.putExtra("number2", 2)
+//            startActivity(intent)
 
             val intent2 = Intent(this@Intent1, Intent2::class.java)
             // Apply
@@ -24,7 +25,19 @@ class Intent1 : AppCompatActivity() {
                 this.putExtra("number1", 1)
                 this.putExtra("number2", 1)
             }
-            startActivity(intent2)
+            startActivityForResult(intent2, 200)
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        if (requestCode == 200) {
+            Log.d("number", "" + requestCode)
+            Log.d("number", "" + resultCode)
+            val result = data?.getIntExtra("result", 0)
+        }
+
+        super.onActivityResult(requestCode, resultCode, data)
+
     }
 }
