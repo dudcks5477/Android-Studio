888fd627efca3e939e82b10c20394defa704452c
dudcks5477 <dudcks5477@naver.com>
2022-08-18 12:03:29 +0900

    Fragment Practice

--- app/src/main/java/com/example/myapplication/FragmentActivity.kt
+++ app/src/main/java/com/example/myapplication/FragmentActivity.kt
@@ -3,12 +3,29 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.fragment.app.FragmentManager
+import kotlinx.android.synthetic.main.activity_framgment.*
 
 class FragmentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_framgment)
         Log.d("life_cycle", "onCreate")
+
+        button.setOnClickListener{
+            // flagment를 동적으로 작동하는 방법
+            val fragmentOne: FragmentOne = FragmentOne()
+            val fragmentManager: FragmentManager = supportFragmentManager
+
+            // Transaction
+            // 작업의 단위 -> 시작과 끝이 있다.
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.container, fragmentOne)
+            fragmentTransaction.commit()
+            // 끝을 내는 방법
+            // commit -> 시간 될 때 해 (좀더 안정적)
+            // commitnow -> 지금 당장해
+        }
     }
 
     override fun onStart() {
